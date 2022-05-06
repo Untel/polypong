b1e4cffabaa0b71196c01861555bdde05674d954
adda-sil <adrien@fernandes.bzh>
2022-05-06 20:46:00 +0200

    Stylish types

--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -16,15 +16,7 @@
 <template setup>
   <flat-surface-shader
     class="login-background"
-    type="svg"
-    :mesh="{
-      diffuse: '#1a41a0', ambient: '#040225', depth: 25,
-      segments: 16, slices: 8, width: 1.2, height: 1.2
-    }"
-    :light="{
-      ambient: '#00a07a', diffuse: '#8baf19', count: 3,
-      draw: true, zOffset: 100, autopilot: false
-    }"
+    v-bind="loginShaders"
   >
     <q-form
       ref="form"
@@ -60,9 +52,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { ref } from '@vue/reactivity';
-import FlatSurfaceShader from 'src/components/FlatSurfaceShader.vue';
 import { useRouter } from 'vue-router';
 
+import FlatSurfaceShader from 'src/components/FlatSurfaceShader.vue';
+import { useLoginShaders } from 'src/utils/shaders';
 
 export default defineComponent({
   name: 'LoginPage',
@@ -74,7 +67,12 @@ export default defineComponent({
       console.log('Submitting form', form, login, password);
       router.push('/');
     };
-    return { onSubmitForm, login, password };
+    return {
+      onSubmitForm,
+      login,
+      password,
+      loginShaders: useLoginShaders()
+    };
   },
 });
 </script>
--- front/src/utils/shaders.ts
+++ front/src/utils/shaders.ts
@@ -1,5 +1,27 @@
-import { Light, Mesh } from 'src/types/shaders'; 
+import { Light, Mesh } from 'src/types/shaders';
 
-export const use42LoginPageShadersValue = () => {
+export const useLoginShaders = () => {
+  const mesh: Mesh = {
+    diffuse: '#1a41a0',
+    ambient: '#040225',
+    depth: 25,
+    segments: 16,
+    slices: 8,
+    width: 1.2,
+    height: 1.2
+  };
 
+  const light: Light = {
+    ambient: '#00a07a',
+    diffuse: '#8baf19',
+    count: 3,
+    draw: true,
+    zOffset: 100,
+    autopilot: false
+  }
+  return {
+    type: process.env.SHADER_DEFAULT_RENDERER || 'svg',
+    mesh,
+    light
+  };
 }
