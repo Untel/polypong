b55b3363b84b0ce829cb8e517e88190d177e3eea
Adrien Fernandes <adrien@fernandes.bzh>
2022-05-13 16:32:51 +0200

    Pending test

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log("down");
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
