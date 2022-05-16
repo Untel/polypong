430f877e20b1369a09db7054cc1faf55434a4a02
Adrien Fernandes <adrien@fernandes.bzh>
2022-05-16 06:51:50 +0200

    Fix it

--- apps/front/src/pages/PongPage.vue
+++ apps/front/src/pages/PongPage.vue
@@ -15,31 +15,30 @@
 </template>
 
 <script lang="ts" setup>
-import io from "socket.io-client"
-import { defineComponent } from "vue";
+// import io from "socket.io-client";
 import { PongGame, PongClientEngine } from '@polypong/game';
-import { Lib } from 'lance-gg';
+// import { Lib } from 'lance-gg';
 
 // const socket = io(`http://localhost:3000`)
 // console.log('Socket', socket)
 
 const options = {
-    traceLevel: Lib.Trace.TRACE_ALL,
-    delayInputCount: 3,
-    scheduler: 'render-schedule',
-    syncOptions: {
-        sync: 'extrapolate',
-        remoteObjBending: 0.8,
-        bendingIncrements: 6
-    },
-    worldSettings: {
-      width: 800,
-      height: 900,
-    },
-    serverURL: 'localhost:3000',
-}
-console.log(PongGame)
+  traceLevel: null,
+  delayInputCount: 3,
+  scheduler: 'render-schedule',
+  syncOptions: {
+    sync: 'extrapolate',
+    remoteObjBending: 0.8,
+    bendingIncrements: 6,
+  },
+  worldSettings: {
+    width: 800,
+    height: 900,
+  },
+  serverURL: 'localhost:3000',
+};
 const gameEngine = new PongGame({ options });
 const clientEngine = new PongClientEngine(gameEngine, options);
+console.log(gameEngine, clientEngine);
 clientEngine.start();
 </script>
