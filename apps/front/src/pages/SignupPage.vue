2a4a371e52b770d2875b1b32a9f56f9c601d7805
Adrien Fernandes <adrien@fernandes.bzh>
2022-05-27 22:57:55 +0200

    Fix it

--- apps/front/src/pages/SignupPage.vue
+++ apps/front/src/pages/SignupPage.vue
@@ -25,7 +25,10 @@
     filled
     label="Email"
     lazy-rules
-    :rules="[ val => val && val.length > 0 || 'Please type something']"
+    :rules="[
+      val => !!val || 'Missing email',
+      isValidEmail
+    ]"
   />
   <q-input
     v-model="name"
@@ -33,7 +36,7 @@
     filled
     label="Username"
     lazy-rules
-    :rules="[ val => val && val.length > 0 || 'Please type something']"
+    :rules="[ val => val && val.length > 2 || 'Username should have at least 2 chars']"
   />
   <q-input
     v-model="password"
@@ -42,7 +45,10 @@
     label="Password"
     :type="showPassword ? 'text' : 'password'"
     lazy-rules
-    :rules="[ val => val && val.length > 0 || 'Please type something']"
+    :rules="[
+      val => !!val || 'Password cannot be empty',
+      val => val.length < 6 || 'Password must be stronger',
+    ]"
   >
     <template v-slot:append>
       <q-icon
@@ -55,11 +61,11 @@
   <q-input
     v-model="repeatPassword"
     class="full-width"
-    filled
+    filled                                                                                                                                                                                  
     label="Repeat password"
     :type="showPassword ? 'text' : 'password'"
     lazy-rules
-    :rules="[ val => val && val.length > 0 || 'Please type something']"
+    :rules="[ val => val === password || 'The password dont match']"
   >
     <template v-slot:append>
       <q-icon
@@ -81,6 +87,7 @@ import { useRouter } from 'vue-router';
 import LogoCoalition from 'src/components/LogoCoalition.vue';
 import CoalitionSelector from 'src/components/CoalitionSelector.vue';
 import { useAuthStore } from 'src/stores/auth';
+import { ValidationRule } from 'quasar';
 
 
 const email           = ref<String>(''),
@@ -101,6 +108,11 @@ const onSubmitForm = async (form: Event) => {
     coalition.value,
   );
 };
+const isValidEmail = (val: string): any => {
+  const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+  return emailPattern.test(val) || 'Seems to be an incorrect email';
+}
+
 const emit = defineEmits(['changeBackground']);
 watch(coalition, (coa) => {
   console.log('Watching it ?', coalition);
