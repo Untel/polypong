029b548581c1efd1896071db3879303b6c4e262e
Enzo Dal Cerro <edal--ce@student.42.fr>
2022-06-13 19:46:25 +0200

    ball bounce side + correct mouse

--- apps/api/src/game/game.class.ts
+++ apps/api/src/game/game.class.ts
@@ -1,31 +1,53 @@
 import Lobby from "./lobby.class";
 import { Server, Socket } from 'socket.io';
 
+
+class Ball {
+  x: number;
+  y: number;
+  velocity: {
+    x: number;
+    y: number;
+  };
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    // console.log(this)
+    this.velocity = { x: 1.5, y: 1 };
+    // this.velocity.y = 0;
+  }
+  bounceTop() {
+    this.velocity.y = -this.velocity.y
+  };
+  bounceSide() {
+    this.velocity.x = -this.velocity.y
+  }
+}
+class Paddle {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
 export default class Game {
   lobby: Lobby;
   socket: Server;
-  ball: {
-    x: number,
-    y: number
-  };
-  paddle1: {
-    x: number,
-    y: number
-  };
-  paddle2: {
-    x: number,
-    y: number
-  };
+
+  ball: Ball;
+  paddle1: Paddle;
+  paddle2: Paddle;
   speed: number = 10;
   constructor(socket: Server, lobby: Lobby) {
 
     this.lobby = lobby;
     this.socket = socket;
 
-    this.ball = { x: 0, y: 0 };
+    this.ball = new Ball;
+    //   x: 0, y: 0, velocity: { x: 1, y: 1 }
+    // }
     // this.ball.y = 0;
-    this.paddle1 = { x: 0, y: 0 };
-    this.paddle2 = { x: 0, y: 0 };
+    this.paddle1 = new Paddle;
+    this.paddle2 = new Paddle;
 
     // this.socket.on('PaddleUpdate', this.updatePaddle);
 
@@ -44,8 +66,17 @@ export default class Game {
   updatePositionPaddles({ y }) {
     this.paddle1.y = y;
   }
-
+  run_physics() {
+    // console.log("ball is : ", this.ball)
+    this.ball.x += this.ball.velocity.x;
+    this.ball.y += this.ball.velocity.y;
+    if (this.ball.x >= 500 - 20 || this.ball.x <= 0)
+      this.ball.bounceSide();
+    if (this.ball.y >= 500 - 20 || this.ball.y <= 0)
+      this.ball.bounceTop();
+  }
   tick() {
-    this.socket.emit('gameUpdate', { ball: this.ball, paddle1: this.paddle1, paddle2: this.paddle2 });
+    this.run_physics();
+    this.socket.emit('gameUpdate', { ball: { x: this.ball.x, y: this.ball.y }, paddle1: this.paddle1, paddle2: this.paddle2 });
   }
 }
--- apps/front/src/pages/GamePage.vue
+++ apps/front/src/pages/GamePage.vue
@@ -5,20 +5,28 @@
   width: 20px;
   height: 100px;
 }
+
+.ball {
+  background-color: red;
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+  position: absolute;
+}
 </style>
 <template>
   <q-page padding>
     <div style="display: flex; justify-content: center; align-items: center;">
       <div style="background-color:white ; width: 500px; height: 500px; position: relative;" ref="world"
         @mousemove="onMouseMove">
-        <div style="background-color: red; width: 20px; height: 20px; display: inline-block; position:absolute;"
-          ref=" ball">a</div>
+        <div class="ball" :style="ballStyle" ref="ball">a</div>
         <div class="paddle" :style="paddle1Style" ref="paddle1">b</div>
         <!-- <div style="position:absolute; background-color: blue; width: 20px; height: 100px;" ref="paddle2">c</div> -->
       </div>
     </div>
     <pre style="background-color: aquamarine;">
-      {{ paddle1Style }}
+      Paddle : {{ paddle1Style }}
+      Ball : {{ ballStyle }}
     </pre>
   </q-page>
 </template>
@@ -30,16 +38,17 @@ import { compileScript } from 'vue/compiler-sfc';
 import CoalitionSelectorVue from 'src/components/CoalitionSelector.vue';
 const { socket } = useAuthStore();
 
-const world = ref<HTMLElement>(),
-  ball_ref = ref<HTMLElement>(),
-  paddle1_ref = ref<HTMLElement>(),
-  paddle2_ref = ref<HTMLElement>();
+// const world = ref<HTMLElement>(),
+//   ball_ref = ref<HTMLElement>(),
+//   paddle1_ref = ref<HTMLElement>(),
+//   paddle2_ref = ref<HTMLElement>();
 
 let ball: any;
 let paddle1: any;
 let paddle2: any;
 
 const paddle1Style = ref<StyleValue>();
+const ballStyle = ref<StyleValue>();
 const props = defineProps({
   lobbyId: Number,
 });
@@ -49,8 +58,16 @@ const onKeyDown = (evt: KeyboardEvent) => {
 }
 
 const onMouseMove = (e: MouseEvent) => {
+
   var rect = e.target.getBoundingClientRect();
-  var y = e.clientY - rect.top;  //y position within the element.
+  console.log(rect)
+
+  console.log(e.clientY);
+  var y = e.clientY - rect.top - 50;  //y position within the element.
+  console.log("y = ", y)
+  y = (y < 0) ? 0 : y;
+  y = (y > 400) ? 400 : y;
+
   socket?.emit('paddleMouseUpdate', { y });
 }
 
@@ -58,13 +75,12 @@ const update = (evt: any) => {
   ball = evt.ball;
   paddle1 = evt.paddle1;
   paddle2 = evt.paddle2;
-
   paddle1Style.value = { top: paddle1.y + 'px', left: paddle1.x + 'px ' }
+  ballStyle.value = { top: ball.y + 'px', left: ball.x + 'px' }
 }
 
 onMounted(() => {
-  console.log(ball_ref.value, world.value, paddle1_ref.value, paddle2_ref.value);
-  // this.socket.subscribe
+  // console.log(ball_ref.value, world.value, paddle1_ref.value, paddle2_ref.value);
   window.addEventListener('keydown', onKeyDown);
 });
 
