f598319b3628294340dbc5dca606c653e51b2dc5
Adrien Fernandes <adrien@fernandes.bzh>
2022-06-21 12:22:29 +0200

    Change paddle movements

--- apps/api/src/game/game.class.ts
+++ apps/api/src/game/game.class.ts
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 03:00:00 by adda-sil          #+#    #+#             */
-/*   Updated: 2022/06/20 20:59:53 by adda-sil         ###   ########.fr       */
+/*   Updated: 2022/06/21 12:21:05 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ import Redis from 'ioredis';
 import RedisBridge from './redis.bridge';
 import Store from 'redis-json';
 import { lineInterpolate, polygonRegular, lineAngle, angleToDegrees, pointOnLine, Line, pointInPolygon, angleReflect, Point } from 'geometric';
-import Polygon from 'polygon';
+import { polygonOffset } from 'polygon';
 import MyPolygon from './polygon.class';
 
 class Position {
@@ -35,39 +35,46 @@ class Position {
 
 class Ball {
   pos: Position;
-  velocity: {
-    x: number;
-    y: number;
-  };
+  speed: number;
+  angle: number;
+  direction: any;
+  lastHitten: any;
+
   constructor() {
     // console.log(this)
     this.pos = new Position(0, 0, 10, 10);
-    this.velocity = { x: .5, y: .5 };
+    this.angle = 0;
+    this.speed = 1;
     // this.velocity.y = 0;
   }
-  bounceTop() {
-    this.velocity.y = -this.velocity.y
-  };
-  bounceSide() {
-    this.velocity.x = -this.velocity.y
+
+  move() {
+    this.pos.x += Math.sin(this.angle) * this.speed;
+    this.pos.y += Math.cos(this.angle) * this.speed;
   }
+
   reset() {
     this.pos.x = 0;
     this.pos.y = 0;
-    this.velocity = {
-      x: getRandomFloatArbitrary(-1, 2),
-      y: getRandomFloatArbitrary(-1, 2),
-    }
+    this.angle = getRandomFloatArbitrary(0, Math.PI * 2);
   }
 }
 class Paddle {
-  pos: Position;
   line: Line;
-  angle: number
-  constructor(x = 0, y = 0, angle = 0, h = 5, w = 100) {
-    this.pos = new Position(x, y, h, w);
-    this.angle = angle;
-    this.line = [[0, 0], [1, 0]];
+  interpolate: Function;
+
+  constructor(axis: Line) {
+    this.interpolate = lineInterpolate(axis)
+    this.updatePercentOnAxis(.5);
+  }
+
+  updatePercentOnAxis(ratio: number) {
+    const newPos = this.interpolate(ratio);
+    const newPosEnd = this.interpolate(ratio + .2);
+    this.line = [
+      newPos,
+      newPosEnd,
+    ];
   }
 }
 
@@ -102,9 +109,9 @@ export default class Game {
     // this.edges = new Polygon(polygonRegular(nEdges, 2000, [50, 50]))
     this.map = new MyPolygon(nPlayers);
     this.balls[0] = new Ball();
-    this.paddles = this.map.edges.map(([a, b], idx) => {
-      console.log("Paddle", a, b);
-      return new Paddle(a[0], b[0], this.map.angles[idx], 5, this.map.edgeWidth / 8);
+    this.paddles = this.map.edges.map((line, idx) => {
+      console.log("Paddle", line);
+      return new Paddle(line);
     });
     this.socket.emit('mapChange', this.networkMap);
     this.socket.emit('gameUpdate', this.networkState);
@@ -122,37 +129,21 @@ export default class Game {
 
 
   updatePaddle(evt: string) {
-    if (evt === 'ArrowUp' && this.paddles[0].pos.y > 0) {
-      this.paddles[0].pos.y -= this.speed;
-    } else if (evt === 'ArrowDown' && this.paddles[0].pos.y < 400) {
-      this.paddles[0].pos.y += this.speed;
-    }
+    // if (evt === 'ArrowUp' && this.paddles[0].pos.y > 0) {
+    //   this.paddles[0].pos.y -= this.speed;
+    // } else if (evt === 'ArrowDown' && this.paddles[0].pos.y < 400) {
+    //   this.paddles[0].pos.y += this.speed;
+    // }
   }
 
   updatePositionPaddles({ y }) {
-    this.paddles[0].pos.y = y;
+    // this.paddles[0].pos.y = y;
   }
 
   updatePaddlePercent(percent: number) {
-    const playerIndex = 0;
-    const paddle = this.paddles[playerIndex];
-    const edge = this.map.edges[playerIndex];
-
-    const newPos = lineInterpolate(edge)(percent);
-    const newPosEnd = lineInterpolate(edge)(percent + .2);
-
-
-    paddle.pos.x = newPos[0];
-    paddle.pos.y = newPos[1];
-    paddle.line = [newPos, newPosEnd];
-    console.log(
-      "Px: " + paddle.pos.x,
-      "Py: " + paddle.pos.y,
-      "lol", this.paddles[playerIndex].pos,
-      // "Edge: ", edge,
-      // "%: " + percent,
-      // "New Pos", newPos
-    );
+    this.paddles.forEach(paddle => {
+      paddle.updatePercentOnAxis(percent);
+    });
   }
 
 
@@ -161,26 +152,27 @@ export default class Game {
     this.balls.forEach(ball => {
       const { x, y } = ball.pos;
       const point: Point = [x, y];
+
+      for (let i = 0; i < this.paddles.length; i++) {
+        // Not good to handle the hit point
+        if (pointOnLine(point, this.paddles[i].line)) {
+          console.log(`Paddle ${i} has been hitten`);
+          // use it https://observablehq.com/@harrystevens/geometric-anglereflect to reflect the ball
+          return ;
+        }
+      }
+
       const pip = pointInPolygon(point, this.map.verticles);
 
       if (!pip) {
-        console.log("Ball not in polygon");
+        // console.log("Ball not in polygon");
         // ball.velocity.x = -ball.velocity.x;
         // ball.velocity.y = -ball.velocity.y;
 
-        for (let i = 0; i < this.paddles.length; i++) {
-          // Not good to handle the hit point
-          if (pointOnLine(point, this.paddles[i].line)) {
-            console.log(`Paddle ${i} has been hitten`);
-            // use it https://observablehq.com/@harrystevens/geometric-anglereflect to reflect the ball
-            return ;
-          }
-        }
 
         ball.reset();
       }
-      ball.pos.x += ball.velocity.x;
-      ball.pos.y += ball.velocity.y;
+      ball.move();
     })
   }
 
