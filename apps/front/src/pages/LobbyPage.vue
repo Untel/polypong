071111a5563f61559525807093a44e0403c57da0
Adrien Fernandes <adrien@fernandes.bzh>
2022-07-18 15:24:52 +0200

    Section lobby

--- apps/front/src/pages/LobbyPage.vue
+++ apps/front/src/pages/LobbyPage.vue
@@ -4,12 +4,43 @@
     Id: {{ props.id }} {{ props }}
     <q-btn @click="start">Start game</q-btn>
     {{ lobby }}
+
+  <section>
+    <q-form
+      @validationSuccess="onFormChange"
+      >
+      <q-slider
+        name="playersMax"
+        v-model="lobbyForm.playersMax"
+        :min="2"
+        :max="16"
+        snap
+        vertical
+        reverse
+        markers
+        label-always
+        label
+      />
+      <q-input v-model="lobbyForm.name" label="Lobby name"
+        name="name"
+        :rules="[ val => val && val.length > 2 || 'Username should have at least 2 chars']"
+      />
+    <q-btn color="primary" type="submit" label="Validate" />
+    </q-form>
+  </section>
+  <section>
+    <article>
+      <code>
+        yolo
+      </code>
+    </article>
+  </section>
   </q-page>
 </template>
 
 <script lang="ts" setup>
 
-import { defineProps, computed } from 'vue';
+import { defineProps, computed, ref, reactive } from 'vue';
 import { useLobbiesStore } from 'src/stores/lobbies.store';
 import { useApi } from 'src/utils/api';
 
@@ -22,13 +53,22 @@ const props = defineProps({
 
 const lobbies = useLobbiesStore();
 
+const lobbyForm = reactive({
+  name: '',
+  playersMax: 8,
+});
+
 const lobby = lobbies.fetchAndJoinLobby(props.id);
 // lobbies.joinLobby(+props.lobbyId);
 
 const start = () => {
   // props.lobbyId =
   // lobby.resolve()
-}
+};
+
+const onFormChange = (evt) => {
+  console.log("Form changed", evt);
+};
 
 const missingPlayers = computed(() => {
   // return (lobby.playerMax - Object.values(lobby.spectateur))
