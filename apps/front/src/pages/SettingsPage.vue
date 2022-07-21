80158363ab4979e78095b8becff7c7761deab726
lspiess <lambert.spiess@gmail.com>
2022-07-21 22:39:38 +0200

    work on avatar change

--- apps/front/src/pages/SettingsPage.vue
+++ apps/front/src/pages/SettingsPage.vue
@@ -2,14 +2,18 @@
   <span>{{authStore.user}}</span>
   <q-page padding>
     <q-card>
-      <pre>name : {{authStore.user.name}}</pre>
+      <pre>current name : {{authStore.user.name}}</pre>
       <q-input v-model="newName"></q-input>
       <q-btn @click="changeName(newName)">change name</q-btn>
-      <!--
-        <pre v-if="authStore.error.area === 'updateName'">
-          {{ authStore.error.message }}
-        </pre>
-      -->
+    </q-card>
+    <br>
+    <q-card>
+      <pre>current avatar :</pre>
+      <img
+        class="h-20 w-20 rounded-full"
+        :src="authStore.user.avatar"
+        style="max-width: 50%; max-height: 50%;"
+      />
     </q-card>
   </q-page>
 </template>
