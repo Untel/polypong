eee8ef38e56a24a6efb30ef15606c7b69419c766
Adrien Fernandes <adrien@fernandes.bzh>
2022-08-15 04:23:40 +0200

    Nginx optimization

--- compose/nginx/entrypoint/ssl.sh
+++ compose/nginx/entrypoint/ssl.sh
@@ -8,4 +8,3 @@ openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
   -out ${SSL_PATH}.cert \
   -keyout ${SSL_PATH}.key \
   -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=adda-sil/CN=${DOMAIN_NAME}/subjectAltName=DNS:${DOMAIN_NAME}/keyUsage=digitalSignature/extendedKeyUsage=serverAuth"
-
---
+++ compose/nginx/entrypoint/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+printf "PRINTING ${DOMAIN_NAME}.conf\n"
+cat /etc/nginx/conf.d/${DOMAIN_NAME}.conf
