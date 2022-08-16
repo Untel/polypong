e2643965fd98b7af827ea7151fa28b8e7914ae3c
lspiess <lambert.spiess@gmail.com>
2022-08-16 20:17:28 +0200

    work on matchcards

--- apps/front/src/components/MatchCard.vue
+++ apps/front/src/components/MatchCard.vue
@@ -1,14 +1,22 @@
 <template>
 <q-card>
-
+  <q-card-section horizontal>
+    <q-card-section>{{ match.id }}</q-card-section>
+    <q-card-section v-for="
+      player in match.players" :key="`player-${player.id}`"
+    >
+    <span>
+      {{ player.user.name }} rank : {{ player.rank }}
+    </span>
+    </q-card-section>
+  </q-card-section>
 </q-card>
 </template>
 
 <script lang="ts" setup>
 import { useAuthStore } from 'src/stores/auth.store';
+import { useSocialStore } from 'src/stores/social.store';
 import { defineComponent, PropType } from 'vue';
-
-const auth = useAuthStore();
 
 defineComponent({ name: 'MatchCard' });
 
@@ -17,6 +25,8 @@ const props = defineProps({
     type: Object as PropType<any>,
   },
 });
+
+const auth = useAuthStore(); const soc = useSocialStore();
 
 // const emit = defineEmits(['matchCardClick']);
 //
