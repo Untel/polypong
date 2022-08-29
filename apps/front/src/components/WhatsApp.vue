01c9fa1a451554d8f9089a8f5e0e8f50ac047a3a
Adrien Fernandes <adrien@fernandes.bzh>
2022-08-29 22:39:06 +0200

    Icons

--- apps/front/src/components/WhatsApp.vue
+++ apps/front/src/components/WhatsApp.vue
@@ -395,9 +395,9 @@ function actionable(target: Participant) : Action[] {
     if (props.me.status >= ThreadMemberStatus.ADMIN
       && target.status < props.me.status) {
       actions.push(
-        { label: 'Kick', icon: { name: 'fas fa-user-large-slash' }, fn: $thread.kick },
-        { label: 'Ban', icon: { name: 'fas fa-user-large-slash' }, fn },
-        { label: 'Mute', icon: { name: 'fas fa-user-large-slash' }, fn: $thread.mute },
+        { label: 'Kick', icon: { name: 'fas fa-person-through-window' }, fn: $thread.kick },
+        { label: 'Ban', icon: { name: 'fas fa-hand-middle-finger' }, fn },
+        { label: 'Mute', icon: { name: 'fas fa-head-side-cough-slash' }, fn: $thread.mute },
       );
     }
 
