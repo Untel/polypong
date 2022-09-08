e37d7080f6bc1ce335d67ce69b860128e8839680
lspiess <lambert.spiess@gmail.com>
2022-09-08 22:03:47 +0200

    blabla

--- apps/front/src/components/LadderBoard.vue
+++ apps/front/src/components/LadderBoard.vue
@@ -35,7 +35,7 @@ import { useAuthStore } from 'src/stores/auth.store';
 import { useMatchHistoryStore } from 'src/stores/history.store';
 import { useSocialStore } from 'src/stores/social.store';
 import StatsCard from 'src/components/StatsCard.vue';
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import { asyncComputed } from '@vueuse/core';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -46,22 +46,36 @@ const $route = useRoute(); const router = useRouter();
 
 defineComponent({ name: 'LadderBoard' });
 
-const usersIds = asyncComputed(async () => {
+const usersIds = ref<number[]>([]);
+
+onMounted(async () => {
   // console.log('0---------------------------------------------');
   const raw = await his.getPlayersUsersIds();
   // console.log('raw = ', raw);
-  const arr: number[] = [];
   raw?.forEach((e) => {
-    arr.push(parseInt(e.user_id, 10));
+    usersIds.value.push(parseInt(e.user_id, 10));
   });
   // console.log('1---------------------------------------------');
-  arr.forEach(async (userId) => {
-    // console.log('fetching history for userId = ', userId);
-    await his.fetchUserMatchesHistory(userId);
-    // console.log('fetched history.stats = ', his.getUserMatchesHistory(userId)?.stats);
-  });
+
+  // for (let i = 0; i < usersIds.value.length; i += 1) {
+  //  // eslint-disable-next-line no-await-in-loop
+  //  await his.fetchUserMatchesHistory(usersIds.value[i]);
+  // }
+
+  const promises = await Promise.all(
+    usersIds.value.map(async (el) => {
+      await his.fetchUserMatchesHistory(el);
+    }),
+  );
+
+  //  usersIds.value.forEach(async (userId) => {
+  //    // console.log('fetching history for userId = ', userId);
+  //    await his.fetchUserMatchesHistory(userId);
+  //    // console.log('fetched history.stats = ', his.getUserMatchesHistory(userId)?.stats);
+  //  });
+
   // console.log('2---------------------------------------------');
-  arr.sort((ida: number, idb: number) => {
+  usersIds.value.sort((ida: number, idb: number) => {
     // console.log('ida = ', ida, ', idb = ', idb);
     const statsa = his.getUserMatchesHistory(ida)?.stats;
     const statsb = his.getUserMatchesHistory(idb)?.stats;
@@ -76,9 +90,43 @@ const usersIds = asyncComputed(async () => {
     }
     return 0;
   });
+  // console.log('after sort, usersIds = ', usersIds.value);
   // console.log('3---------------------------------------------');
-  return arr;
 });
+
+// const usersIds = asyncComputed(async () => {
+//  console.log('0---------------------------------------------');
+//  const raw = await his.getPlayersUsersIds();
+//  console.log('raw = ', raw);
+//  const arr: number[] = [];
+//  raw?.forEach((e) => {
+//    arr.push(parseInt(e.user_id, 10));
+//  });
+//  console.log('1---------------------------------------------');
+//  arr.forEach(async (userId) => {
+//    console.log('fetching history for userId = ', userId);
+//    await his.fetchUserMatchesHistory(userId);
+//    console.log('fetched history.stats = ', his.getUserMatchesHistory(userId)?.stats);
+//  });
+//  console.log('2---------------------------------------------');
+//  arr.sort((ida: number, idb: number) => {
+//    console.log('ida = ', ida, ', idb = ', idb);
+//    const statsa = his.getUserMatchesHistory(ida)?.stats;
+//    const statsb = his.getUserMatchesHistory(idb)?.stats;
+//    console.log('in sort, statsa = ', statsa, ', statsb= ', statsb);
+//    const ra = statsa ? statsa.ratio : 0;
+//    const rb = statsb ? statsb.ratio : 0;
+//    console.log('in sort, ra = ', ra, ', rb = ', rb);
+//    if (ra < rb) {
+//      return 1;
+//    } if (ra > rb) {
+//      return -1;
+//    }
+//    return 0;
+//  });
+//  console.log('3---------------------------------------------');
+//  return arr;
+// });
 
 const emit = defineEmits(['click']);
 
