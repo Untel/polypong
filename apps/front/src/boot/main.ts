7fac8eb33f9e5ea4fa869b7ed6540f291731c36b
Adrien Fernandes <adrien@fernandes.bzh>
2022-09-12 10:39:21 +0200

    Fix default positions notify

--- apps/front/src/boot/main.ts
+++ apps/front/src/boot/main.ts
@@ -1,10 +1,13 @@
 import { boot } from 'quasar/wrappers';
+import { Notify } from 'quasar';
 import 'src/libs/fss';
 
 export default boot(({ app }) => {
   // eslint:disable-next-line
   app.config.globalProperties.$env = {};
-
+  Notify.setDefaults({
+    position: 'bottom-right',
+  });
   const originalFetch = fetch;
   window.fetch = function (url, init?) {
     // url = url.toString().replace('g.tenor.com/v1', 'tenor.googleapis.com/v2');
